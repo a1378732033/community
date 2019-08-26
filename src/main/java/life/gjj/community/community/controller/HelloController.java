373c22ef33e78f444283bf7985831d280991a2c3
gujiajia <a1378732033@qq.com>
2019-08-26 16:23:18 +0800

    init repo

---
+++ src/main/java/life/gjj/community/community/controller/HelloController.java
@@ -0,0 +1,16 @@
+package life.gjj.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hell(@RequestParam(name="name") String name, Model model){
+      model.addAttribute("name",name);
+        return "hello";
+    }
+}
